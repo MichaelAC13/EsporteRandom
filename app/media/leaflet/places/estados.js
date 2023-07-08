adc973e3f75ada5d2135d3ab062b59db5b170e28
Michael <michaell.r.almeida@gmail.com>
2023-07-08 15:06:35 -0300

    Mapa

---
+++ app/media/leaflet/places/estados.js
@@ -0,0 +1,221 @@
+﻿export const Estados = [
+	{
+		"codigo_uf": 12,
+		"uf": "AC",
+		"nome": "Acre",
+		"latitude": -15.2483,
+        "longitude": -58.1259,
+		"regiao": "Norte"
+	},
+	
+	{
+		"codigo_uf": 27,
+		"uf": "AL",
+		"nome": "Alagoas",
+		"latitude": -9.66599,
+        "longitude": -35.735,
+		"regiao": "Nordeste"
+	},
+	{
+		"codigo_uf": 16,
+		"uf": "AP",
+		"nome": "Amapá",
+		"latitude": 0.034934,
+        "longitude": -51.0694,
+		"regiao": "Norte"
+	},
+	{
+		"codigo_uf": 13,
+		"uf": "AM",
+		"nome": "Amazonas",
+		"latitude": -3.11866,
+        "longitude": -60.0212,
+		"regiao": "Norte"
+	},
+	{
+		"codigo_uf": 29,
+		"uf": "BA",
+		"nome": "Bahia",
+		"latitude": -12.9718,
+        "longitude": -38.5011,
+		"regiao": "Nordeste"
+	},
+	{
+		"codigo_uf": 23,
+		"uf": "CE",
+		"nome": "Ceará",
+		"latitude": -18.944,
+        "longitude": -46.6669,
+		"regiao": "Nordeste"
+	},
+	{
+		"codigo_uf": 53,
+		"uf": "DF",
+		"nome": "Distrito Federal",
+		"latitude": -15.7795,
+        "longitude": -47.9297,
+		"regiao": "Centro-Oeste"
+	},
+	{
+		"codigo_uf": 32,
+		"uf": "ES",
+		"nome": "Espírito Santo",
+		"latitude": -26.1674,
+        "longitude": -51.231,
+		"regiao": "Sudeste"
+	},
+	{
+		"codigo_uf": 52,
+		"uf": "GO",
+		"nome": "Goiás",
+		"latitude": -16.6864,
+        "longitude": -49.2643,
+		"regiao": "Centro-Oeste"
+	},
+	{
+		"codigo_uf": 21,
+		"uf": "MA",
+		"nome": "Maranhão",
+		"latitude": -2.53874,
+        "longitude": -44.2825,
+		"regiao": "Nordeste"
+	},
+	{
+		"codigo_uf": 51,
+		"uf": "MT",
+		"nome": "Mato Grosso",
+		"latitude": -15.601,
+        "longitude": -56.0974,
+		"regiao": "Centro-Oeste"
+	},
+	{
+		"codigo_uf": 50,
+		"uf": "MS",
+		"nome": "Mato G. do Sul",
+		"latitude": -9.95542,
+        "longitude": -36.7926,
+		"regiao": "Centro-Oeste"
+	},
+	{
+		"codigo_uf": 31,
+		"uf": "MG",
+		"nome": "Minas Gerais",
+		"latitude": -19.9102,
+        "longitude": -43.9266,
+		"regiao": "Sudeste"
+	},
+	{
+		"codigo_uf": 15,
+		"uf": "PA",
+		"nome": "Pará",
+		"latitude": -1.4554,
+        "longitude": -48.4898,
+		"regiao": "Norte"
+	},{
+		"codigo_uf": 25,
+		"uf": "PB",
+		"nome": "Paraíba",
+		"latitude": -7.11509,
+        "longitude": -34.8641,
+		"regiao": "Nordeste"
+	},
+	{
+		"codigo_uf": 41,
+		"uf": "PR",
+		"nome": "Paraná",
+		"latitude": -25.4195,
+        "longitude": -49.2646,	
+		"regiao": "Sul"
+	},
+	{
+		"codigo_uf": 26,
+		"uf": "PE",
+		"nome": "Pernambuco",
+		"latitude": -8.04666,
+        "longitude": -34.8771,
+		"regiao": "Nordeste"
+	},
+	{
+		"codigo_uf": 22,
+		"uf": "PI",
+		"nome": "Piauí",
+		"latitude": -5.09194,
+        "longitude": -42.8034,
+		"regiao": "Nordeste"
+	},
+	{
+		"codigo_uf": 33,
+		"uf": "RJ",
+		"nome": "Rio de Janeiro",
+		"latitude": -22.9129,
+        "longitude": -43.2003,
+		"regiao": "Sudeste"
+	},
+	{
+		"codigo_uf": 24,
+		"uf": "RN",
+		"nome": "Rio G. do Norte",
+		"latitude": -5.79357,
+        "longitude": -35.1986,
+		"regiao": "Nordeste"
+	},
+	{
+		"codigo_uf": 43,
+		"uf": "RS",
+		"nome": "Rio G. do Sul",
+		"latitude": -30.0318,
+        "longitude": -51.2065,
+		"regiao": "Sul"
+	},
+	{
+		"codigo_uf": 11,
+		"uf": "RO",
+		"nome": "Rondônia",
+		"latitude": -8.76077,
+        "longitude": -63.8999,
+		"regiao": "Norte"
+	},
+	
+	{
+		"codigo_uf": 14,
+		"uf": "RR",
+		"nome": "Roraima",
+		"latitude": -8.79766,
+        "longitude": -39.8241,
+		"regiao": "Norte"
+	},
+	{
+		"codigo_uf": 42,
+		"uf": "SC",
+		"nome": "Santa Catarina",
+		"latitude": -27.5945,
+        "longitude": -48.5477,
+		"regiao": "Sul"
+	},
+	{
+		"codigo_uf": 35,
+		"uf": "SP",
+		"nome": "São Paulo",
+		"latitude": -23.5329,
+        "longitude": -46.6395,
+		"regiao": "Sudeste"
+	},
+	{
+		"codigo_uf": 28,
+		"uf": "SE",
+		"nome": "Sergipe",
+		"latitude": -10.9091,
+        "longitude": -37.0677,
+		"regiao": "Nordeste"
+	},
+	{
+		"codigo_uf": 17,
+		"uf": "TO",
+		"nome": "Tocantins",
+		"latitude": -26.4839,
+        "longitude": -51.9888,
+		"regiao": "Norte"
+	},
+]
+
+export default Estados
